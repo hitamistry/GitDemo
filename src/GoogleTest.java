55accacda7b94b0b8bca92f8da2c24a653ba5b31
hita <kavu26mistry@gmail.com>
2023-05-15 22:24:03 +0530

    first commit

---
+++ src/GoogleTest.java
@@ -0,0 +1,30 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class GoogleTest {
+
+	
+	@Test
+	public void HomePageCheck() throws MalformedURLException {
+		
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setBrowserName("chrome");
+//		caps.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+//		caps.setCapability(CapabilityType.BROWSER_NAME, "chrome");
+//		caps.setAcceptInsecureCerts(true);
+		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.43.116:4444"), caps);
+		driver.get("http://google.com");
+		System.out.println(driver.getTitle());
+		driver.findElement(By.name("q")).sendKeys("rahul shetty");
+		driver.close();
+	}
+	
+	
+}
---
+++ src/RSATest.java
@@ -0,0 +1,30 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class RSATest {
+
+	
+	@Test
+	public void HomePageCheck() throws MalformedURLException {
+		
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setBrowserName("firefox");
+//		caps.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+		caps.setCapability(CapabilityType.BROWSER_NAME, "firefox");
+//		caps.setAcceptInsecureCerts(true);
+		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.43.116:4444"), caps);
+		driver.get("http://rahulshettyacademy.com");
+		System.out.println(driver.getTitle());
+		driver.findElement(By.name("q")).sendKeys("rahul shetty");
+		driver.close();
+	}
+	
+	
+}
